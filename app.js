4052c0449b2d4f216eae046cd5fff52b663e8fee
begly <service.alex@begly.co.uk>
2014-11-15 20:08:50 +0000

    Initial server setup

---
+++ app.js
@@ -0,0 +1,22 @@
+var config  = require('./config.json');
+var express = require('express');
+var app = express();
+
+var server = app.listen(config.port)
+
+var io = require('socket.io').listen(server);
+
+var router = express.Router();
+
+app.use('/', router);
+
+app.use(express.static(__dirname + '/public'));
+
+router.get('/', function(req, res) {
+	res.sendfile(__dirname + '/public/html/index.html');
+});
+
+// about page route (http://localhost:8080/about)
+router.get('/about', function(req, res) {
+	res.send('im the about page!');
+});
