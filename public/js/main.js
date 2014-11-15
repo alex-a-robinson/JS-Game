6cebf298810b318d965ced676b20ee62ec53aeb2
begly <service.alex@begly.co.uk>
2014-11-15 20:37:07 +0000

    Add player class with basic movement

--- public/js/main.js
+++ public/js/main.js
@@ -25,6 +25,16 @@ $(function() {
     ctx = canvas.getContext("2d");
 
     drawFloor();
+
+
+    var p1 = new Player('Me', {x: 100, y: 100}, {width: 50, height: 50}, '#FF0000');
+    p1.draw();
+
+    $(document).click(function() {
+        p1.move();
+        drawFloor();
+        p1.draw();
+    })
 })
 
 // Draw a rect across the entire canvas
@@ -37,6 +47,26 @@ function drawFloor() {
     ctx.fillRect(0, 0, width, height);
 }
 
+var Player = function(name, pos, size, colour) {
+    this.name = name;
+    this.pos = pos;
+    this.size = size;
+    this.colour = colour;
+
+    this.speed = 2;
+    this.angle = Math.PI/2;
+}
+
+Player.prototype.draw = function() {
+    ctx.fillStyle = this.colour;
+    ctx.fillRect(this.pos.x, this.pos.y, this.size.width, this.size.height);
+}
+
+Player.prototype.move = function() {
+    this.pos.x += Math.sin(this.angle) * this.speed;
+    this.pos.y -= Math.cos(this.angle) * this.speed;
+}
+
 function log(msg, level) {
 
     if (!level)
@@ -45,11 +75,15 @@ function log(msg, level) {
     switch (level) {
         case 'DEBUG':
             console.log(msg);
+            break;
         case 'INFO':
             console.info(msg);
+            break
         case 'WARN':
             console.warn(msg);
+            break;
         case 'ERROR':
             console.error(msg);
+            break;
     }
 }
